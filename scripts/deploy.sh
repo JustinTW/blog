77074e2861c50aa92930b0508544952f5b6d3f77
Justin CT Liu <justintwd@gmail.com>
2018-07-06 11:20:38 +0800

    fix: fix travis.yml syntax

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,11 @@
 
 DEPLOY_ENV=${1-staging}
 
+echo $DEPLOY_ENV
+echo $FTP_HOST
+echo $FTP_USER
+echo $FTP_PASS
+
 cp packages/blog/_config.sample.yml packages/blog/_config.yml
 sed -i "s/FTP_HOST/${FTP_HOST}/" packages/blog/_config.yml
 sed -i "s/FTP_USER/${FTP_USER}/" packages/blog/_config.yml
@@ -10,6 +15,8 @@ sed -i "s/FTP_PASS/${FTP_PASS}/" packages/blog/_config.yml
 if [ $DEPLOY_ENV == "production" ]; then
   sed -i "s/staging//" packages/blog/_config.yml
 fi
+
+cat packages/blog/_config.yml
 
 make build
 make publish -C packages/blog
